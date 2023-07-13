b3ce15567aa35aa60c655658398dc0ad1e6b76df
Vatsal01 <1906371@kiit.ac.in>
2023-07-13 12:12:52 +0530

    First commit

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Onion',
+    image: '/images/airpods.jpg',
+    description:
+      'Onions are not only delicious and help enhance the taste of food, but they also possess healing properties and have a number of nutritional benefits. Onions aid in digestion, enhances the skin and they also help protect against cold, allergies and cough.',
+    brand: 'Farm Grown',
+    category: 'Vegetable',
+    price: 32,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Mango',
+    image: '/images/phone.jpg',
+    description:
+      'Mango is a delicious seasonal fruit that bears a red tinge on the top like sindoor. It has a pleasant sweet taste with mild tartness. Because of its delightful flavour, it is often used in the preparation of juices, shakes, squashes, jams, jellies and candies.',
+    brand: 'Organically Grown',
+    category: 'Fruit',
+    price: 65,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Potato',
+    image: '/images/camera.jpg',
+    description:
+      'Potatoes are a good source of fiber, which can help you lose weight by keeping you full longer. Fiber can help prevent heart disease by keeping cholesterol and blood sugar levels in check. Potatoes are also full of antioxidants that work to prevent diseases and vitamins that help your body function properly.',
+    brand: 'Farm Grown',
+    category: 'Vegetable',
+    price: 25,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Banana',
+    image: '/images/playstation.jpg',
+    description:
+      'Banana is medium sized, tastes sweet and has a sugary aroma and a soft pulp. It has a dark green skin that turns yellow on ripening. Robusta bananas are eaten raw, used in baking, fruit salads, fruit compotes, and to complement foods.',
+    brand: 'Farm Grown',
+    category: 'Fruit',
+    price: 40,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Tomato',
+    image: '/images/mouse.jpg',
+    description:
+      'Tomatoes are a significant source of umami flavor.The tomato is consumed in diverse ways, raw or cooked, in many dishes, sauces, salads, and drinks. While tomatoes are fruits—botanically classified as berries—they are commonly used culinarily as a vegetable ingredient or side dish.',
+    brand: 'Farm Grown',
+    category: 'Vegetable',
+    price: 40,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cauliflower',
+    image: '/images/alexa.jpg',
+    description:
+      'Cauliflower is a cruciferous vegetable that is naturally high in fiber and B-vitamins. It provides antioxidants and phytonutrients that can protect against cancer. It also contains fiber to enhance weight loss and digestion, choline that is essential for learning and memory, and many other important nutrients.',
+    brand: 'Organically Grown',
+    category: 'Vegetable',
+    price: 42,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
